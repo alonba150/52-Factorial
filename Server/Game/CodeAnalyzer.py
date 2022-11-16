17208c070c5ad36f8c9ae98b15ed20aef4e16c75
Ran Perry <ranperry1@gmail.com>
2022-11-16 15:51:31 +0200

    Server-advanced-analyzer initial commit
    Contains a basic framework of the nodes and how they should work

--- Server/Game/CodeAnalyzer.py
+++ Server/Game/CodeAnalyzer.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from Utils.Event import Event
 
 
@@ -6,37 +8,8 @@ class CodeAnalyzer:
         self.game = game
         self.__initialize_commands()
 
-    def read_code(self, code: str, node_next=(lambda *args, **kwargs: None)):
-        if code.isdigit():
-            return [Node(lambda _node_next, *args, **kwargs: _node_next(int(code)), 0, node_next)]
-        cmd_t = code[0]
-        cmd_i = code[1] + code[2] + code[3]
-        node = Node(*self.__find_dict[cmd_t][int(cmd_i)], node_next)
-        cmds = code[5:-1]
-        if not cmds:
-            return [node]
-        starter_i = 0
-        for index in [i for i, c in enumerate(cmds) if c == ',']:
-            temp = cmds[starter_i: index]
-            if temp.count('(') == temp.count(')'):
-                starter_i = index
-                cmds = cmds[0: index] + '%' + cmds[index+1:]
-        cmds = cmds.split('%')
-        print(cmds)
-        results = []
-        for sub_cmd in cmds:
-            if res := self.read_code(sub_cmd, node): results.extend(res)
-        return results
-
     def analyze_code(self, code: str):
-        activate = Event()
-        i = 0
-        for node in self.read_code(code):
-            if i >= 0:
-                print(node)
-                activate += node
-            i += 1
-        return activate
+        Node.initialize_nodes(list(map(lambda c: c.split(': '), code.split('///'))), self.__find_dict)
 
     def __initialize_commands(self):
         # region THE BIG SWITCH CASE
@@ -74,37 +47,75 @@ class CodeAnalyzer:
 
 
 class Node:
-    def __init__(self, cmd, arg_l: int, node_next):
-        self.cmd = cmd
-        self.arg_l = arg_l
-        self.args = []
-        self.repeatables = []
-        self.node_next = node_next
+    nodes = {}
+    starter_nodes = []
+    func_dict = {}
 
-    def __call__(self, *args, static=True):
-        #print(self)
-        if not static:
-            self.repeatables.append(args[0])
-            if 1 + len(self.args) == self.arg_l:
-                for arg in self.repeatables:
-                    self.cmd(*([arg] + self.args), self.node_next, static=static)
-                    self.repeatables.remove(arg)
-        elif len(args) == self.arg_l:
-            self.cmd(*args, self.node_next, static=static)
-        elif len(args) == 1:
-            self.args.append(args[0])
-            if len(self.args) == self.arg_l:
-                self.cmd(self.args[1], self.args[0], self.node_next, static=static)
-            elif 1 + len(self.args) == self.arg_l:
-                for arg in self.repeatables:
-                    self.cmd(*([arg] + self.args), self.node_next, static=static)
+    @staticmethod
+    def initialize_nodes(nodes_data: List[str], func_dict: dict):
+        """
+        Manages all the nodes accordingly to create a functioning web of them
+        :param nodes_data: string list containing id and data for each node
+        :param func_dict: a dict containing all the command functions
+        """
+        Node.func_dict = func_dict
+        nodes = []
+        for node_data in nodes_data: nodes.append(Node(node_data[0]))
+        for node_i in range(len(nodes)): nodes[node_i].set_attr(nodes_data[node_i])
+        for node in Node.nodes.values(): node.do_static()
 
-    def __repr__(self):
-        return \
-            f"""Node:
-    {self.cmd =}
-    {self.arg_l =}
-    {self.args =}
-    {self.repeatables =}
-"""
+    def __init__(self, node_id):
+        self.__id = node_id
+        # Adds self to the dict of nodes by ids
+        Node.nodes[self.id] = self
 
+        self.__func = None
+        self.__inputs = []
+        self.__outputs = []
+        self.static = 0
+
+    @property
+    def id(self) -> int:
+        return self.__id
+
+    @property
+    def func(self):
+        return self.__func
+
+    def set_attr(self, data: str) -> bool:
+        """
+        Changes this node's data from a string containing both it's id
+        and other information
+        :param data: string containing the information
+        :return: function success boolean
+        """
+        if data.count(': ') != 1: return False
+        self.__id, node_data = data.split(': ')
+        # Reads data from string with some string manipulation
+        node_data = node_data[1:-1]  # Removing brackets []
+        func_code, inputs, outputs = node_data.split('*')
+        self.__func = Node.func_dict[func_code[0]][int(func_code[1])]  # Get function from string code (FE: A000)
+
+        # Add node as starter node if node is type A
+
+    def initialize_connections(self) -> bool:
+        pass
+
+    def has_value(self, output_index):
+        if output_index >= len(self.__outputs):
+            return False
+        return self.__outputs[output_index]
+
+    def pop_value(self, output_index):
+        if output_index >= len(self.__outputs):
+            return False
+        if len(self.__outputs[output_index]) > 0:
+            return self.__outputs[0] if self.static else self.__outputs.pop(0)
+        return False
+
+    def do_static(self):
+        self.static = all(input_node[0].static for input_node in self.__inputs)
+
+    def __call__(self, *args, **kwargs):
+        if all(input_node[0].has_value(input_node[1]) for input_node in self.__inputs):
+            self.func(*[input_node[0].pop_value(input_node[1]) for input_node in self.__inputs])
